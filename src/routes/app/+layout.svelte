0a836494d0e71ec04ab9763bc236ec5a19a3a257
Lukasz Piszka <96567570+Piszczek999@users.noreply.github.com>
2026-01-03 17:55:39 +0100

    feat: add logo and favicon

--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
 	import LevelProgressBar from "$lib/components/LevelProgressBar.svelte";
 	import UserPopover from "$lib/components/UserPopover.svelte";
+	import icon from "$lib/assets/logo.png";
 
 	const { children } = $props();
 </script>
 
-<nav class="absolute top-0 flex w-full items-center justify-between gap-4 p-4 px-8">
-	<div class="text-2xl">
-		<a href="/">Daily League</a>
+<nav class="absolute top-0 flex w-full items-center justify-between gap-4 p-4 px-2 md:px-8">
+	<div>
+		<a href="/" class="flex items-center gap-2 text-3xl"
+			><img src={icon} alt="" class="h-12 w-12 rounded-lg" />Daily League
+		</a>
 	</div>
 	<div class="flex items-center gap-4 rounded-full p-2 shadow-glow shadow-white">
 		<LevelProgressBar />
@@ -15,6 +18,6 @@
 	</div>
 </nav>
 
-<main class="flex-center min-h-screen flex-col pt-16">
+<main class="flex-center min-h-screen flex-col pt-20">
 	{@render children()}
 </main>
